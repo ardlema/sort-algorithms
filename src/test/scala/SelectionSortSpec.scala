efedac977a073615d81a75c8de121f83fc3a6341
Alberto Rodriguez <arodriguez@stratio.com>
2016-10-03 13:12:33 +0200

    Modify properties name

--- src/test/scala/SelectionSortSpec.scala
+++ src/test/scala/SelectionSortSpec.scala
@@ -1,9 +1,9 @@
 import org.scalacheck.Properties
 import org.scalacheck.Prop.forAll
 
-object SelectionSortSpec extends Properties("String") {
+object SelectionSortSpec extends Properties("Selection Sort") {
 
-  property("startsWith") = forAll { (l1: List[Int]) =>
+  property("Sort properly") = forAll { (l1: List[Int]) =>
     SelectionSort.sort(l1) == l1.sorted
   }
 }
